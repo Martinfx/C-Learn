27de12e529391bf86ae274a4076049bfd2167a52
maficccc@gmail.com <maficccc@gmail.com>
2017-05-27 14:31:44 +0200

    Added change source code

--- array/main.c
+++ array/main.c
@@ -3,14 +3,14 @@
 int main(int argc, char *argv[])
 {
     // Static lenght array
-    char str1[] = "Hello world";
-    printf("%s %c %d %d %d\n", str1, str1[0], &str1, &str1[0], str1);
+    char string[0] = "Hello world";
+    printf("%s %c %d %d %d\n", string, string[0], &string, &string[0], string);
 
 
     int *p = NULL;
-    int a[] = {200,600,900,2000};
-    p = a;
-    for (unsigned int i = 0; i < sizeof (a) / sizeof (a[0]); i++)
+    int arr[] = {200,600,900,2000};
+    p = arr;
+    for (unsigned int i = 0; i < sizeof (arr) / sizeof (arr[0]); i++)
     {
         printf("Value = %d, Address = %p\n", *p, p);
         p = p + 1;
