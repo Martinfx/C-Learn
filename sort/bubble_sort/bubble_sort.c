d336051da2213b6618546bd52722d0a80c337809
maficccc@gmail.com <maficccc@gmail.com>
2017-05-28 23:11:15 +0200

    Added print values

--- sort/bubble_sort/bubble_sort.c
+++ sort/bubble_sort/bubble_sort.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+#define size_array(x) (int)( sizeof(x) / sizeof(x[0]) )
 
-void bubblesort(int *arr)
+void bubblesort(int arr[], int size)
 {
     int helper = 0;
-    for(int x = 0; x < 12 - 1; x++)
+    for(int x = 0; x < size - 1; x++)
     {
-        for(int i = 0; i < 12 - 1 ; i++)
+        for(int i = 0; i < size - 1 ; i++)
         {
             if(arr[i] > arr[i + 1])
             {
@@ -21,13 +22,13 @@ void bubblesort(int *arr)
 /// This is simple sort algoritm bubble sort
 int main(int argc, char *argv[])
 {
-    int arr[12] = { 102,252,669,22,223,33,93,36,3,0,222,99};
+    int arr[] = { 102,252,669,22,223,33,93,36,3,0,222,99};
 
-    bubblesort(arr);
+    bubblesort(arr, size_array(arr));
 
-    for(int z = 0; z < 12; z++)
+    for(int z = 0; z < size_array(arr); z++)
     {
-        printf("Sorted: %d \n", arr[z]);
+        printf("Sorted element: %d, value: %d \n", z, arr[z]);
     }
 
     return 0;
