634098215e557880b5f493a49e310fa4818f1bd9
maficccc@gmail.com <maficccc@gmail.com>
2017-06-15 18:47:32 +0200

    Added pointer to struct

---
+++ Pointer_to_struct.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int x, y;
+    double z;
+    long long int big;
+
+} test_struct;
+
+
+int main()
+{
+    test_struct *p = NULL, *q = NULL;
+
+    printf("Size of test_struct: %lu\n", sizeof(test_struct));
+
+    p = (test_struct*)calloc(4, sizeof (test_struct));
+
+    for(int i = 0; i < 4; i++)
+    {
+        p[i].x = i;
+        p[i].y = i * 20;
+        p[i].z = 100000.0 + i;;
+        p[i].big = 4294967296 + i;
+
+        printf("[%d] values:\n x is %d\n y is %d\n z is %f\n big is %lld\n",
+               i, p[i].x, p[i].y, p[i].z, p[i].big);
+    }
+    //q = p;
+
+    //printf("[0] values:\n x is %d\n y is %d\n z is %d\n big is %lld\n",
+    //       q->x, q->y, q->z, q->big);
+
+    //q = q + 2;
+
+    //printf("[2] values:\n x is %d\n y is %d\n z is %d\n big is %lld\n",
+    //       q->x, q->y, q->z, q->big);
+
+    free(p);
+
+    return 0;
+}
---
+++ Pointer_to_struct.pro
@@ -0,0 +1,6 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += Pointer_to_struct.c
