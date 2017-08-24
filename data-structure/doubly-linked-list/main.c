f50a749e9b59bad9773283d8536302651cce5b54
maficccc@gmail.com <maficccc@gmail.com>
2017-08-24 14:05:54 +0200

    Fixed name structure

--- data-structure/doubly-linked-list/main.c
+++ data-structure/doubly-linked-list/main.c
@@ -6,42 +6,41 @@
  * Allows operations push, pop, remove, delete item.
  */
 
-typedef struct double_linked_list
+typedef struct doubly_linked_list
 {
     int node_data;
-    struct double_linked_list *next;
-    struct double_linked_list *prev;
+    struct doubly_linked_list *next;
+    struct doubly_linked_list *prev;
 
-} double_linked_list_t;
+} doubly_linked_list_t;
 
-double_linked_list_t* create_double_linked_list(int n)
+doubly_linked_list_t* create_double_linked_list(int n)
 {
-    double_linked_list_t *head = NULL;
-    double_linked_list_t *temp = NULL;
+    doubly_linked_list_t *head = NULL;
+    doubly_linked_list_t *temp = NULL;
 
-    head->next = head;
-    head->prev = head;
+   //x head->next = head;
+    //head->prev = head;
+
     //head->node_data = -1;
-
     //printf("list item : current is %p; next is %p; prev is %p;\n", (void*)temp, (void*)temp->next, (void*)temp->prev);
 
     if(n > 0)
     {
         for(int i = 0; i < n; i++)
         {
-            temp = (double_linked_list_t*)malloc(sizeof(double_linked_list_t));
+            temp = (doubly_linked_list_t*)malloc(sizeof(doubly_linked_list_t));
             if(temp == NULL)
             {
-                printf("Pointer is NULL: ");
+                printf("Allocation memory failed!");
                 return NULL;
-
             }
 
             temp->node_data = i;
-            temp->next = head->next;
-            head->next = temp;
+            temp->next = head;
+            //head->next = temp;
             temp->prev = head;
-            temp->next->prev = temp;
+            //temp->next->prev = temp;
 
             printf("Next item: ");
 
@@ -51,9 +50,9 @@ double_linked_list_t* create_double_linked_list(int n)
     return head;
 }
 
-void print_list(double_linked_list_t *head)
+void print_list(doubly_linked_list_t *head)
 {
-    double_linked_list_t *temp = NULL;
+    doubly_linked_list_t *temp = NULL;
 
     temp = head->next;
     while (temp != head)
@@ -66,8 +65,9 @@ void print_list(double_linked_list_t *head)
 int main()
 {
     //printf("xxxxxxxxxxxxxxxxxxxxxxx");
-    //double_linked_list_t *list = NULL;
-    //list = create_double_linked_list(5);
+    doubly_linked_list_t *list = NULL;
+    list = create_double_linked_list(4);
+
     //print_list(list);
 
     return 0;
