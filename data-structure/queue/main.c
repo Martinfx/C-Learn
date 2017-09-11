839c1104baa1e8c3c5d7640737f02cea9446dee8
Martinfx <maficccc@gmail.com>
2017-09-11 18:49:27 +0200

    Added fix enqueue, print_queue

--- data-structure/queue/main.c
+++ data-structure/queue/main.c
@@ -8,7 +8,7 @@
 
 typedef struct d_list
 {
-    size_t node_data;
+    int node_data;
     struct d_list *next;
     struct d_list *prev;
 } node_t;
@@ -20,7 +20,7 @@ typedef struct queue
     int size;
 } queue_t;
 
-node_t *create_node(n)
+node_t *create_node(int n)
 {
     node_t *head = (node_t*)malloc(sizeof(node_t));
     if(head == NULL)
@@ -32,6 +32,7 @@ node_t *create_node(n)
     head->next = NULL;
     head->prev = NULL;
     head->node_data = n;
+    printf("Create node data: %d \n", head->node_data);
     return head;
 }
 
@@ -47,6 +48,7 @@ queue_t* create_queue()
     temp->first = NULL;
     temp->last  = NULL;
     temp->size++;
+    printf("Create queue: %d \n", temp->size);
     return temp;
 }
 
@@ -59,9 +61,27 @@ void enqueue(queue_t *queue, int n)
         queue->last  = node;
     }
 
-    queue->last->next = node;
-    queue->last->prev = queue->last->next;
+    printf("Added node to queue \n");
+
+    node->prev = queue->last;
+    node->prev->next = node;
     queue->last = node;
+}
+
+void print_queue(queue_t *queue)
+{
+    if(queue->first == NULL && queue->last == NULL)
+    {
+        printf("Queue is empty! \n");
+        return;
+    }
+
+    node_t *temp = queue->first;
+    while(temp != NULL)
+    {
+        printf("Queue data: %d", temp->node_data);
+        temp = temp->next;
+    }
 }
 
 int main()
@@ -70,4 +90,8 @@ int main()
     q = create_queue();
 
     enqueue(q, 1);
+    //enqueue(q, 2);
+
+
+    //print_queue(q);
 }
