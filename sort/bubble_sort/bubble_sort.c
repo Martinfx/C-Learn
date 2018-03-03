af1515a90f26eca5b76926dc867028fd7a3c7385
maficccc@gmail.com <maficccc@gmail.com>
2018-03-03 22:20:10 +0100

    revision bubble sort

--- sort/bubble_sort/bubble_sort.c
+++ sort/bubble_sort/bubble_sort.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define size_array(x) (int)(sizeof(x) / sizeof(x[0]))
 
 void bubblesort(int arr[], int size)
 {
-    for(int x = 0; x < size - 1; x++)
-    {
-        for(int i = 0; i < size - x -  1; i++)
-        {
-            if(arr[i] > arr[i + 1])
-            {
+    bool swap = false;
+    do {
+        swap = false;
+        for(int i = 0; i < size - 1; ++i) {
+            if(arr[i] > arr[i + 1]) {
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
+                swap = true;
             }
         }
-    }
+    }while(swap);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-    int arr[] = { 102,252,669,22,223,33,93,36,3,0,222,99, 1100, 121122,2,6, -10, -20, -52};
+    int arr[] = {102,252,669,22,223, -55, -150, 0, 1};
 
     bubblesort(arr, size_array(arr));
 
-    for(int z = 0; z < size_array(arr); z++)
+    for(int i = 0; i < size_array(arr); ++i)
     {
-        printf("Sorted element: %d, value: %d \n", z, arr[z]);
+        printf("Sorted element: %d, value: %d \n", i, arr[i]);
     }
 
     return 0;
