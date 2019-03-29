84eff16cbc38fb7d51a50772680c3606f32add15
Martinfx <maficccc@gmail.com>
2019-03-30 00:07:54 +0100

    Add example array with makefile

---
+++ data-structures/array_makefile/array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array.h"
+
+int main() {
+    printf("Hello makefile");
+    return 0;
+}
---
+++ data-structures/array_makefile/array.h
@@ -0,0 +1,7 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
