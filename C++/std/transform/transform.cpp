677a261b0837ac11dd8455902436f293a0986bd2
Martinfx <maficccc@gmail.com>
2019-07-17 21:28:23 +0200

    simple changes in vector

--- C++/std/transform/transform.cpp
+++ C++/std/transform/transform.cpp
@@ -2,25 +2,27 @@
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <random>
 
 int main()
 {
-    std::vector<int32_t> num1 = {1,3,5,7,9,11,13,15,17,19};
-    std::vector<int32_t> num2 = {2,4,6,8,10,12,14,16,18,20};
-    std::vector<int32_t> vector_result;
+    std::vector<int32_t> vec1, vec2, result;
     std::list<int32_t> list_result;
 
+    vec1.emplace_back(std::rand() % 100);
+    vec2.emplace_back(std::rand() % 100);
+
     // std::vector havent push_front, only we use std::back_inserter
-    std::transform(num1.begin(), num1.end(),
-                   num2.begin(), std::back_inserter(vector_result),
+    std::transform(vec1.begin(), vec1.end(),
+                   vec2.begin(), std::back_inserter(result),
                    [](int32_t i, int32_t j){ return i + j;});
 
-    for(auto i : vector_result) {
+    for(auto i : result) {
         std::cout << "Vector result: " << i << std::endl;
     }
 
-    std::transform(num1.begin(), num1.end(),
-                   num2.begin(), std::front_inserter(list_result),
+    std::transform(vec1.begin(), vec1.end(),
+                   vec2.begin(), std::front_inserter(list_result),
                    [](int32_t i, int32_t j){ return i + j;});
 
     for(auto i : list_result) {
--- C++/std/vector/vector.cpp
+++ C++/std/vector/vector.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <algorithm>
 
-int main()
-{
-    std::vector<int> vec = {12,2333,4445,555,554433,6666,77888,55,5676};
+int main() {
+    std::vector<int> vec;
+    vec.emplace_back(std::rand() % 100000);
 
     std::for_each(vec.begin(), vec.end(), [](auto &v) {
         std::cout << v << std::endl;
@@ -15,13 +15,15 @@ int main()
         std::cout << "x: " << x << std::endl;
     }
 
-    std::vector<std::string> vec1 = {"go", "stable", "release", "configure"};
+    std::vector<std::string> vec1 = {"goto","go",
+                                     "stable", "release",
+                                     "configure"};
+
     for(auto v : vec1) {
        std::cout  << v << std::endl;
     }
 
     std::for_each(vec1.begin(), vec1.end(), [](const std::string& str) {
-        //std::cout << "---------------" << std::endl;
         std::cout << "std::for_each: " << str << std::endl;
     });
 
