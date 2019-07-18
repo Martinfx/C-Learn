0117a9a81c03a468076ce3df745af13d8a8dfa90
Martinfx <maficccc@gmail.com>
2019-07-18 17:02:54 +0200

    Change example for 'statistic'

--- C++/statistic/MeanMedianMode/main.cpp
+++ C++/statistic/MeanMedianMode/main.cpp
@@ -5,44 +5,40 @@
 #include <map>
 #include <algorithm>
 
-double mean(std::vector<double> mean)
-{
-    double m_mean = 0.0;
-    for(unsigned int i = 0; i < mean.size(); ++i)
-    {
-        m_mean += mean[i];
+template<typename T>
+T mean(std::vector<T> mean) {
+    T result = 0.0;
+    for (auto i : mean) {
+         result += mean[i];
     }
-
-    return m_mean / mean.size();
+    return result / mean.size();
 }
 
-double median(std::vector<double> &median)
-{
-    double m_median = 0.0;
-    std::sort(median.begin(), median.begin() + median.size());
-    m_median = median[(median.size() / 2 ) - 1] + median[(median.size() / 2 )];
 
-    if(median.size() % 2 == 0)
-    {
+template<typename T>
+T median(std::vector<T> &median) {
+    T result;
+    std::sort(median.begin(), median.end());
+    result = median[(median.size() / 2 ) - 1] + median[(median.size() / 2 )];
+
+    if(median.size() % 2 == 0) {
        return (median[(median.size() / 2 ) - 1] + median[(median.size() / 2 )])  / 2.0;
     }
 
     return median[(median.size()  / 2) - 1];
 }
 
-double mode(std::vector<double> mode)
-{
-    std::map<double, int> m_numbers;
-    for(unsigned int i = 0; i < mode.size(); ++i)
-    {
-        m_numbers.insert(std::pair<double, int>(mode[i], 0));
-        if(mode[i])
-        {
-            m_numbers.at(mode[i]) += 1;
+template <typename T>
+T mode(std::vector<T> mode) {
+    std::map<double, int> result;
+    for(unsigned int i = 0; i < mode.size(); ++i) {
+        result.insert(std::pair<double, int>(mode[i], 0));
+        if(mode[i]) {
+            result.at(mode[i]) += 1;
         }
     }
 
-    auto x = std::max_element(m_numbers.begin(), m_numbers.end(),
+    auto x = std::max_element(result.begin(), result.end(),
     [](const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
         return p1.second < p2.second; });
 
@@ -50,19 +46,10 @@ double mode(std::vector<double> mode)
 }
 
 int main() {
-    int n = 0;
-    std::vector<double> num;
-    std::cin >> n;
-    for(int i = 0; i < n; ++i)
-    {
-        int numbers = 0;
-        std::cin >> numbers;
-        num.push_back(numbers);
-    }
-
-    std::cout << mean(num) << std::endl;
-    std::cout << median(num) << std::endl;
-    std::cout << mode(num) << std::endl;
+    std::vector<double> numbers {11.0,222.0,2225.0,111.0,22.0,11.0,22.0};
+    std::cout << mean(numbers) << std::endl;
+    std::cout << median(numbers) << std::endl;
+    std::cout << mode(numbers) << std::endl;
 
     return 0;
 }
