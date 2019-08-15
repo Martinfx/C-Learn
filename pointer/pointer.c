05f194d6595430dfee718e6a96fe378b48a5c237
Martinfx <maficccc@gmail.com>
2019-08-15 19:35:04 +0200

    fix address for printf

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -6,17 +6,22 @@ int main(int argc, char **argv)
     /// int variables
     int number = 0;
     int number_two = 0;
-
+    int number_tree = 0;
     /// Pointer variables
     int* pointer = NULL;
     int* pointer_two = NULL;
+
+    int* pointer_tree = NULL;
 
     number = 100;
     pointer = &number;
     pointer_two = pointer;
     number_two = *pointer;
+    pointer_tree = pointer;
 
-    printf("num=%d, numPtr=%d, address of num=%d, num2=%d, numPtr2=%d\n", number, pointer, &number, number_two, pointer);
+    number_tree = *pointer_tree;
+
+    printf("num=%d, numPtr=%p, address of num=%d, num2=%d, numPtr2=%p\n, num3=%d", number, pointer, &number, number_two, pointer_two, number_tree);
 
     return 0;
 }
