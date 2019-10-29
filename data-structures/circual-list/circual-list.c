1c035ae85b5cbc2fce84d13df356ab7069472143
User Maxfx <maficccc@gmail.com>
2019-10-29 09:36:02 +0100

    update circual list

---
+++ data-structures/circual-list/circual-list.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node {
+    struct node *head;
+    struct node *tail;
+    struct node *next;
+    int key;
+} c_list_t;
+
+c_list_t *create_clist() {
+    c_list_t *list = (c_list_t*)malloc(sizeof(c_list_t));
+    // NULL pointer is always false
+    if(!list) {
+        printf("Cannot allocate list!\n");
+        exit(1);
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->next = NULL;
+    list->key = 0;
+
+    return list;
+}
+
+c_list_t *create_node() {
+    c_list_t *node = (c_list_t*)malloc(sizeof(c_list_t));
+    if(!node) {
+        printf("Cannot allocate node!\n");
+        exit(1);
+    }
+
+    node->key = 0;
+    node->next = NULL;
+    return node;
+}
+
+c_list_t *push_front(c_list_t *list, int key) {
+    c_list_t *node = create_node();
+    // if is list->head == NULL then add node
+    if(!list->head) {
+        node->key = key;
+        list->head = node;
+        list->tail = node;
+        node->next = list->head;
+        return list;
+    }
+
+    c_list_t *temp = list->head;
+    node->key = key;
+    node->next = temp;
+    list->head = node;
+    list->tail->next = list->head;
+    return list;
+}
+
+/*c_list_t *push_front__(c_list_t *list, int key) {
+    c_list_t *node = create_node();
+    if(!list->head){
+        node->key = key;
+        list->head = node;
+        list->tail = node;
+        node->next = list->head;
+        return list;
+    }
+
+    c_list_t *temp = list->head;
+    node->key = key;
+    node->next = temp;
+    list->head = node;
+    list->tail->next = list->head;
+    return list;
+}*/
+
+void print_list(c_list_t *list) {
+    if(list->head) {
+        c_list_t *temp = list->head;
+        do {
+            printf("List node: current is %p; \n"
+                   "temp->next is %p; \n"
+                   "temp->data is %d \n",
+                   //"lenght is %d\n",
+                   (void*)temp,
+                   (void*)temp->next,
+                   temp->key);
+            temp = temp->next;
+        } while (temp != list->head);
+    }
+
+    /*while(temp != list->head) {
+            printf("List node: current is %p; \n"
+                   "temp->next is %p; \n"
+                   "temp->data is %d \n",
+                   //"lenght is %d\n",
+                   (void*)temp,
+                   (void*)temp->next,
+                   temp->key);
+
+            temp = temp->next;
+        }*/
+}
+
+/*
+void remove_whole_list(circual_list_t *list) {
+    circual_list_t *temp = list->head;
+    circual_list_t *next = list->head;
+
+    while(temp != NULL) {
+        next = temp->next;
+        free(temp);
+        temp = next;
+    }
+    free(list);
+    list = NULL;
+}
+*/
+
+int main() {
+    c_list_t *list = create_clist();
+    list = push_front(list, 1);
+    list = push_front(list, 2);
+    list = push_front(list, 3);
+    list = push_front(list, 4);
+    list = push_front(list, 5);
+    list = push_front(list, 6);
+
+    print_list(list);
+    printf("--------------------------------------");
+
+
+    list = push_front(list, 7);
+    list = push_front(list, 8);
+    list = push_front(list, 9);
+
+    print_list(list);
+    printf("--------------------------------------");
+
+    return 0;
+}
+
