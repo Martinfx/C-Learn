a8905c9507c8f6fc11f16ff4ab44f862d8af123a
User Maxfx <maficccc@gmail.com>
2022-06-17 19:04:56 +0200

    pointer: next example

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -4,25 +4,32 @@
 int main(int argc, char **argv)
 {
     /// int variables
-    int number = 0;
-    int number_two = 0;
-    int number_tree = 0;
+    int val, val2, val3 = 0;
+    int sum, mul = 0;
     /// Pointer variables
-    int* pointer = NULL;
-    int* pointer_two = NULL;
+    int *p, *p2, *p3 = NULL;
 
-    int* pointer_tree = NULL;
+    val = 100;
+    // adress to p
+    p = &val;
+    // p2 point to p
+    p2 = p;
+    // give me value from p
+    val2 = *p;
+    // p3 point to p
+    p3 = p;
+    // give me value from p3
+    val3 = *p3;
+    // sumary two pointer
+    sum = *p + *p2;
+    // multiple value plus value pointer
+    mul = sum * (*p);
 
-    number = 100;
-    pointer = &number;
-    pointer_two = pointer;
-    number_two = *pointer;
-    pointer_tree = pointer;
-
-    number_tree = *pointer_tree;
-
-    printf("num=%d, numPtr=%p, address of num=%d, num2=%d, numPtr2=%p\n, num3=%d", number, pointer, &number, number_two, pointer_two, number_tree);
-
+    printf("num=%d, p->%p, address of num=%p\n", val, p, &val);
+    printf("num2=%d, p2->%p, num3=%d\n", val2, p2, val3);
+    printf("sum=%d\n", sum);
+    printf("mul=%d\n", mul);
+    printf("sum=%d", *p += 1);
     return 0;
 }
 
