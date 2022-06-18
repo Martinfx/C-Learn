6396b9cc59bcb0da5e3f690b47dd28cc3538159c
User Maxfx <maficccc@gmail.com>
2022-06-18 20:22:40 +0200

    stack: new stack

--- Data-Structure/C/stack.h
+++ Data-Structure/C/stack.h
@@ -6,43 +6,41 @@
 #include <string.h>
 
 typedef struct stack {
-  struct stack *base;
-  struct stack *end;
-  struct stack *top;
-  uint32_t capacity;
-  uint32_t args;
+    struct stack *base;
+    uint32_t *top;
+    uint32_t capacity;
 } stack_t;
 
 void stack_init(stack_t *s, uint32_t capacity) {
-  s->base = (stack_t *)calloc(5, capacity * sizeof(stack_t));
-  if (!s->base) {
-    exit(0);
-  }
-
-  s->top = NULL;
-  s->end = NULL;
-  s->capacity = capacity;
-  s->end = s->base + s->capacity;
-  s->top = s->base;
+    s->base = (stack_t*)calloc(3, capacity * sizeof (stack_t));
+    s->top = s->base;
+    s->capacity = capacity;
 }
 
-void stack_push(stack_t *s, uint32_t args) {
-  if (s->top == s->end) {
-    printf("Stack is full!");
-  } else {
-    s->top->args = args;
-    printf("Added item to stack: %u \n", s->top->args);
-    s->top += sizeof(uint32_t);
-  }
+void stack_push(stack_t *s, uint32_t val) {
+    if(s->base + s->capacity == s->top) {
+       printf("Stack is full\n");
+    }
+    else {
+        *s->top = val;
+        printf("Added item to stack: %d \n", *s->top);
+        s->top += sizeof(uint32_t);
+    }
 }
 
-void stack_pop(stack_t *s) {
-  if (s->top == s->base) {
-    printf("Stack is empty! \n");
-  }
+uint32_t stack_pop(stack_t *s) {
+    if(s->base == s->top) {
+        printf("Stack is empty!\n");
+        return 0;
+    } else {
+       s->top -= sizeof(uint32_t);
+       printf("Pop this value: %d\n",*s->top);
+       return *s->top;
+    }
+}
 
-  s->top -= sizeof(uint32_t);
-  printf("Pop item: %u \n", s->top->args);
+uint32_t stack_capacity(stack_t* s) {
+    return s->capacity;
 }
 
 void stack_free(stack_t *s) {
@@ -51,3 +49,51 @@ void stack_free(stack_t *s) {
     s->base = NULL;
   }
 }
+
+//typedef struct stack {
+//    struct stack *base;
+//    struct stack *end;
+//    struct stack *top;
+//    uint32_t capacity;
+//    uint32_t args;
+//} stack_t;
+
+//void stack_init(stack_t *s, uint32_t capacity) {
+//    s->base = (stack_t *)calloc(5, capacity * sizeof(stack_t));
+//    if (!s->base) {
+//        exit(0);
+//    }
+
+//    s->top = NULL;
+//    s->end = NULL;
+//    s->capacity = capacity;
+//    s->end = s->base + s->capacity;
+//    s->top = s->base;
+//}
+
+//void stack_push(stack_t *s, uint32_t args) {
+//    if (s->top == s->end) {
+//        printf("Stack is full!");
+//    } else {
+//        s->top->args = args;
+//        printf("Added item to stack: %u \n", s->top->args);
+//        s->top += sizeof(uint32_t);
+//    }
+//}
+
+//void stack_pop(stack_t *s) {
+//    if (s->top == s->base) {
+//        printf("Stack is empty! \n");
+//    }
+
+//    s->top -= sizeof(uint32_t);
+//    printf("Pop item: %u \n", s->top->args);
+//}
+
+//void stack_free(stack_t *s) {
+//    if (s->base) {
+//        free(s->base);
+//        s->base = NULL;
+//    }
+//}
+
--- Data-Structure/C/test_stack.c
+++ Data-Structure/C/test_stack.c
@@ -1,25 +1,17 @@
 #include "stack.h"
 
 int main() {
-    /*stack_t *stack = create_stack();
-    push(stack, 10);
-    push(stack, 20);
-    pop(stack);
-    pop(stack);
-    pop(stack);
-    remove_stack(stack);*/
-
     stack_t stack;
-    stack_init(&stack, 5);
-    stack_push(&stack, 20);
-    stack_push(&stack, 21);
-    stack_push(&stack, 22);
-    stack_push(&stack, 23);
-    stack_push(&stack, 24);
+    stack_init(&stack, 2);
+    for(int i  = 0; i < 3; i++){
+        stack_push(&stack, i);
+    }
+    //stack_push(&stack, 24);
     //stack_push(&stack, 25);
     //stack_push(&stack, 26);
     //stack_push(&stack, 27);
 
+    stack_pop(&stack);
     stack_pop(&stack);
     stack_pop(&stack);
     stack_pop(&stack);
