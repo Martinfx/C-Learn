6b4cd8c5e34064896a3ef9042d3e77b3b4d89576
User Maxfx <maficccc@gmail.com>
2022-06-20 19:43:54 +0200

    circlist: fix function

--- Data-Structure/C/test_circlist.c
+++ Data-Structure/C/test_circlist.c
@@ -3,12 +3,9 @@
 int main() {
     // TODO: Fix me
     clist_t *list = create_list();
-    push_front(list, 10);
-    push_front(list, 11);
-    push_front(list, 12);
-    push_front(list, 13);
-    push_front(list, 14);
+    for(uint32_t i = 0; i < 10; i++) {
+        list = push_front(list, rand() % 100);
+    }
     print_list(list);
-
     return 0;
 }
