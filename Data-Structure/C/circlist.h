0e03d0f2849aa90164a7d8d936d27c75b6291679
User Maxfx <maficccc@gmail.com>
2022-06-28 23:10:58 +0200

    circlist: new version

--- Data-Structure/C/circlist.h
+++ Data-Structure/C/circlist.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <assert.h>
+#include <memory.h>
 
 typedef struct node {
     uint32_t value;
@@ -15,17 +16,25 @@ typedef struct clist {
 } clist_t;
 
 void clist_init(clist_t *clist) {
-    clist->tail = clist->head;
+    clist->tail = NULL;
+    clist->head = NULL;
 }
 
 void clist_push_front(clist_t *clist, uint32_t value) {
     node_t *node = (node_t*)malloc(sizeof(node_t));
+    if (!node) {
+      printf("Allocation memory failed!\n");
+      exit(0);
+    }
+
     node->value = value;
 
-    if(clist->head == clist->tail){
+    if(clist->head == NULL && clist->tail == NULL){
         clist->head = clist->tail = node;
         node->next = clist->head;
+        printf("1xxx\n");
     } else {
+        printf("2xxx\n");
         clist->tail->next = node;
         clist->tail = node;
         node->next = clist->head;
@@ -35,13 +44,27 @@ void clist_push_front(clist_t *clist, uint32_t value) {
 void clist_free(clist_t *clist) {
     node_t *temp = clist->head;
     node_t *next = NULL;
-
     while (temp != NULL) {
       next = temp->next;
       free(temp);
       temp = next;
     }
 }
+
+void print_forward_list(clist_t *list) {
+    node_t *node = list->head;
+    printf("----------------------------------------------------------------------\n");
+    while (node != NULL) {
+      printf("List item: current is %p; next is %p; data is %u\n", (void *)node,
+             (void *)node->next, node->value);
+
+      if(node == list->tail)
+         break;
+
+      node = node->next;
+    }
+  }
+
 
 /*
 typedef struct node {
--- Data-Structure/C/test_circlist.c
+++ Data-Structure/C/test_circlist.c
@@ -3,31 +3,12 @@
 int main() {
     clist_t clist;
     clist_init(&clist);
-    clist_push_front(&clist,1);
-    clist_push_front(&clist,2);
-    clist_free(&clist);
-//    clist_push_front(&clist,3);
-//    clist_push_front(&clist,4);
-
-    // TODO: Fix me
-/*    clist_t *list = create_list();
     for(uint32_t i = 0; i < 10; i++) {
-        list = push_front(list, rand() % 100);
+      clist_push_front(&clist,i);
     }
-    print_list(list);*/
+
+    print_forward_list(&clist);
+    //print_list(&clist);
     return 0;
 }
 
-//AddressSanitizer:DEADLYSIGNAL
-//=================================================================
-//==70485==ERROR: AddressSanitizer: BUS on unknown address (pc 0x0000002b7c0c bp 0x7fffffffd840 sp 0x7fffffffd800 T0)
-//==70485==The signal is caused by a READ memory access.
-//==70485==Hint: this fault was caused by a dereference of a high value address (see register values below).  Disassemble the provided pc to learn which register was used.
-//    #0 0x2b7c0c in push_front /usr/home/maxfx/Documents/C-Learn/Data-Structure/C/circlist.h:33:22
-//    #1 0x2b7c0c in main /usr/home/maxfx/Documents/C-Learn/Data-Structure/C/test_circlist.c:7:16
-//    #2 0x23529f in _start /usr/src/lib/csu/amd64/crt1_c.c:75:7
-//    #3 0x8002dd007  (<unknown module>)
-
-//AddressSanitizer can not provide additional info.
-//SUMMARY: AddressSanitizer: BUS /usr/home/maxfx/Documents/C-Learn/Data-Structure/C/circlist.h:33:22 in push_front
-//==70485==ABORTING
