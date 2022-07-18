355a9791a006ef7d1c0cd79a6e7493e2ffe1ec64
User Maxfx <maficccc@gmail.com>
2022-07-18 20:53:09 +0200

    priority_queue: fix first priority object

--- Data-Structure/C/priority_queue.h
+++ Data-Structure/C/priority_queue.h
@@ -12,22 +12,34 @@ typedef struct node {
 
 typedef struct priority_queue {
     node_t *head;
+    node_t *tail;
 } priority_queue_t;
 
-void priority_queue_init(priority_queue_t *pq) { pq->head = NULL;}
-uint32_t priority_queue_top(priority_queue_t *pq) { return pq->head->item; }
+void priority_queue_init(priority_queue_t *pq) {
+    pq->head = NULL;
+}
+
+uint32_t priority_queue_top(priority_queue_t *pq) {
+    return pq->head->item;
+}
+
 void priority_queue_push(priority_queue_t *pq, uint32_t item, int32_t priority){
     node_t *node = (node_t*)malloc(sizeof(node_t));
+    if (!node) {
+        printf("Allocation memory failed!\n");
+        exit(0);
+    }
     node->item = item;
     node->next = NULL;
     node->priority = priority;
 
-    if(pq->head == NULL || pq->head->priority > priority ) {
+    if(pq->head == NULL || node->priority <= pq->head->priority) {
+        node->next = pq->head;
         pq->head = node;
     }
     else {
         node_t *temp = pq->head;
-        while(temp->next !=NULL && temp->next->priority < priority)
+        while(temp->next !=NULL && temp->next->priority < node->priority)
         {
             temp = temp->next;
         }
@@ -44,6 +56,11 @@ void priority_queue_print(priority_queue_t *pq) {
         printf("%d %d\n", temp->item, temp->priority);
         temp = temp->next;
     }
+
+    /*while (temp != NULL) {
+        printf("%p -> %p, %d, %d\n", (void*)temp, (void*)temp->next,temp->item, temp->priority);
+        temp = temp->next;
+    }*/
 }
 
 void priority_queue_pop(priority_queue_t* pq) {
--- Data-Structure/C/test_priority_queue.c
+++ Data-Structure/C/test_priority_queue.c
@@ -4,9 +4,14 @@ int main(int argc, char *argv[])
 {
     priority_queue_t pq;
     priority_queue_init(&pq);
-    priority_queue_push(&pq, 1, 5);
+    priority_queue_push(&pq, 1, 2);
     priority_queue_push(&pq, 2, 22);
     priority_queue_push(&pq, 3, 10);
+    priority_queue_push(&pq, 4, 5);
+    priority_queue_push(&pq, 5, 3);
+    priority_queue_push(&pq, 6, 10);
+    priority_queue_push(&pq, 7, 1);
+
     priority_queue_print(&pq);
     printf("top: %d \n", priority_queue_top(&pq));
     priority_queue_pop(&pq);
